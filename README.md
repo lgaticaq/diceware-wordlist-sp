43ba7724253239bd15d34fe19df44f0992e66616
Leonardo Gatica <lgatica@protonmail.com>
2015-10-30 01:22:09 -0300

    Add spanish words

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-diceware-wordlist-en
-=========
+# diceware-wordlist-sp
 
-An english wordlist for the diceware password generation method
+[![npm version](https://img.shields.io/npm/v/diceware-wordlist-sp.svg?style=flat-square)](https://www.npmjs.com/package/diceware-wordlist-sp)
+[![npm downloads](https://img.shields.io/npm/dm/diceware-wordlist-sp.svg?style=flat-square)](https://www.npmjs.com/package/diceware-wordlist-sp)
+[![Build Status](https://img.shields.io/travis/lgaticaq/diceware-wordlist-sp.svg?style=flat-square)](https://travis-ci.org/lgaticaq/diceware-wordlist-sp)
+[![devDependency Status](https://img.shields.io/david/dev/lgaticaq/diceware-wordlist-sp.svg?style=flat-square)](https://david-dm.org/lgaticaq/diceware-wordlist-sp#info=devDependencies)
+
+An spanish wordlist for the diceware password generation method
 
 ## Installation
 
-  npm install diceware-wordlist-en --save
+```bash
+npm i -S diceware-wordlist-sp
+```
 
 ## Usage
 
-  var dw_en = require('diceware-wordlist-en');  
-  dw_en['34553'];		# 'jolly'
+```javascript
+const dwSp = require('diceware-wordlist-sp');  
+dwSp['34553']; // 'festa'
+```
--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
-var expect = require('chai').expect,
-	_ = require('lodash'),
-	combinatorics = require('js-combinatorics'),
-    scapegoat = require('../index');
+var expect = require('chai').expect;
+var _ = require('lodash');
+var combinatorics = require('js-combinatorics');
+var words = require('../index');
 
 describe('wordlist', function() {
   it('should have 7776 entries', function() {
-    expect(_.keys(scapegoat).length).to.equal(7776);
+    expect(_.keys(words).length).to.equal(7776);
   });
 
   it('should have all possible 5-dice rolls', function() {
-  	var cmb = combinatorics.baseN(['1','2','3','4', '5', '6'], 5);
-	var cnt = 0;
-	while(dice = cmb.next()) {
-		dice_str = dice.join('');
-		expect(scapegoat[dice_str], "combination: "+dice_str).to.be.a('string');
-	}
+  	var cmb = combinatorics.baseN(['1', '2', '3', '4', '5', '6'], 5);
+  	var cnt = 0;
+  	while(dice = cmb.next()) {
+  		diceStr = dice.join('');
+  		expect(words[diceStr], "combination: " + diceStr).to.be.a('string');
+  	}
   });
 
   it('should have unique words', function() {
-  	var values = _.values(scapegoat);
+  	var values = _.values(words);
   	var unique = _.uniq(values);
     expect(values.length).to.equal(_.uniq(values).length);
   });
